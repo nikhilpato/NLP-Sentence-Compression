11ac958ea7bce9ab87c7ed52fe32f6888669dbaa
Roberto Dominguez <Dominguez7777@gmail.com>
2019-12-06 07:06:46 -0800

    create app structure

---
+++ app/sentence-compression-app/src/components/Content/Content.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Content.module.css';
+
+function Content() {
+    return(
+        <div className={styles.content_container}>
+            <div className={styles.input_container}>
+
+            </div>
+            <div className={styles.process_container}>
+
+            </div>
+            <div className={styles.results_container}>
+
+            </div>
+        </div>
+    )
+}
+
+export default Content;
